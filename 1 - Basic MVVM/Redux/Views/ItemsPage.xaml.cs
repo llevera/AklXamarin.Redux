37bce419899daf2b750135eee1b46b5effb8fd88
andrewrevell <arevell@fraedom.com>
2019-07-27 16:33:03 +1200

    Cleanup

---
+++ 1 - Basic MVVM/Redux/Views/ItemsPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Redux.Models;
+using Redux.Views;
+using Redux.ViewModels;
+
+namespace Redux.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = viewModel = new ItemsViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
