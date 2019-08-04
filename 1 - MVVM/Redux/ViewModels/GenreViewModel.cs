b069d92c4b1dd6c756d9166072c61d4775600de4
andrewrevell <arevell@fraedom.com>
2019-08-04 21:28:31 +1200

    Cleanup

--- 1 - MVVM/Redux/ViewModels/GenreViewModel.cs
+++ 1 - MVVM/Redux/ViewModels/GenreViewModel.cs
@@ -7,8 +7,6 @@ namespace Redux.ViewModels
     {
         private readonly Genre _genre;
 
-        private int _genreUpvotes;
-
         public GenreViewModel(Genre genre, int votes)
         {
             _genre = genre;
@@ -19,16 +17,8 @@ namespace Redux.ViewModels
 
         public bool ShowVotes => Votes != 0;
 
-        public int Votes
-        {
-            get => _genreUpvotes;
-            set
-            {
-                _genreUpvotes = value;
-                OnPropertyChanged(nameof(Votes));
-            }
-        }
-
+        public int Votes { get; }
+ 
         public Color TextColor
         {
             get
